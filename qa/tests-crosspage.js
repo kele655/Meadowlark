9042a4b2a4a62c4861f0659b5c926075aa6a9498
Inam <inam.ahmadzada@gmail.com>
2016-11-05 08:19:30 +0000

    Chapter 4 materials version 1

---
+++ qa/tests-crosspage.js
@@ -0,0 +1,41 @@
+var Browser = require("zombie"), assert = require("chai").assert;
+
+var browser;
+
+suite('Cross-Page Tests', function(){
+    
+    setup(function(){
+        browser = new Browser();
+    });
+    
+    test('requesting a group rate quote from the hood river tour page' +
+        'should populate the referrer field', function(done){
+           var referrer = 'https://node-express-programmer655.c9users.io/tours/hood-river';
+           browser.visit(referrer, function(){
+               browser.clickLink('.requestGroupRate', function(){
+                   assert(browser.field('referrer').value === referrer);
+                   done();
+               });
+           });
+    });
+    
+    test('requesting a group rate from the oregon coast tour page should ' +
+        'populate the referrer field', function(done) {
+        var referrer = 'https://node-express-programmer655.c9users.io/tours/oregon-coast';
+        browser.visit(referrer, function(){
+            browser.clickLink(.requestGroupRate, function(){
+                assert(browser.field('referrer').value === referrer);
+                done();
+            });
+        });
+    });
+    
+    test('visiting the "request group rate" page dirctly should result ' +
+        'in an empty referrer field', function(done){
+        browser.visit('https://node-express-programmer655.c9users.io/tours/request-group-rate',
+            function(){
+                assert(browser.field('referrer').value === '');
+                done();
+            });
+        });
+});
